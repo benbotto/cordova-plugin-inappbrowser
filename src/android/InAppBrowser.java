34d3760276ccb31db4782a0791b5ec7d132ce400
Ben Botto <benjamin.botto@gmail.com>
2017-02-27 14:44:59 -0800

    Fixes CB-11248: InAppBrowser no focus on input text fields.

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -931,6 +931,10 @@ public class InAppBrowser extends CordovaPlugin {
         public void onPageFinished(WebView view, String url) {
             super.onPageFinished(view, url);
 
+            // CB-11248 Cannot enter text into text box.
+            view.clearFocus();
+            view.requestFocus();
+
             // CB-10395 InAppBrowser's WebView not storing cookies reliable to local device storage
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
                 CookieManager.getInstance().flush();
